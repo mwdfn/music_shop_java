a56f4f07bb50d41eae915a261f0518e5047e128e
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-26 16:33:11 +0100

    add InstrumentType enum class

--- src/main/java/instruments/InstrumentType.java
+++ src/main/java/instruments/InstrumentType.java
@@ -2,11 +2,17 @@ package instruments;
 
 public enum InstrumentType {
 
-    STRING1,
-    STRING2,
-    WOODWIND1,
-    BRASS1,
-    BRASS2;
+    STRING1("guitar", "string"),
+    STRING2("banjo", "string"),
+    WOODWIND1("flute", "woodwind"),
+    BRASS1("trumpet", "brass"),
+    BRASS2("french horn", "brass");
 
-    
+    private final String name;
+    private final String family;
+
+    InstrumentType(String name, String family) {
+        this.name = name;
+        this.family = family;
+    }
 }
